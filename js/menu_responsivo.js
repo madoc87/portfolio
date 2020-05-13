fb0e8ddb158bd97dbd0792b0879ed00802ef1047
Fabricio Assis <fabricio.r.assis@gmail.com>
2020-05-13 13:08:53 -0300

    Correcoes SonarQube
    Add Fonte genericas, exclusao de codigos duplicados, correcao do link google fonts, add lang nas paginas html, substituicoes das tags <i> por <em>, add atributo noopener e noreferrer aos links com target blank para previnir vunerabilidades, remocao do script menu_scroll, add alt nas imgens.

--- js/menu_responsivo.js
+++ js/menu_responsivo.js
@@ -1,37 +1,25 @@
 //Verifica o redimencionamento da tela < 900px entao adiciona a classe nav-overflow para ativar o menu mobile-nav 
 $( window ).resize(function() {
 	var width = $(window).width();
-	console.log(width);
 	var el = document.getElementById("html");
-
 	if(width < 901){
-
   		el.classList.add("nav-overflow");
-
 	}else{
   		el.classList.remove("nav-overflow");
 	 }
-	 
 });
-
 $(function(){
-
 	// Verificar tamanho da tela < 900px entao adiciona a classe nav-overflow para ativar o menu mobile-nav 
 	var width = $(document).width();
 	var el = document.getElementById("html");
 	if(width < 901){
-
   		el.classList.add("nav-overflow");
-
 	}else{
   		el.classList.remove("nav-overflow");
 	 }
-
 	var opMenu = false;
-
 	// Desloca com animate para a direita o menu mobile e o body em 300px
 	$('.mobile-button').click(function(){
-
 		if(opMenu == false){
 			opMenu = true;
 			$('#mobile-nav').animate({left: "+=300"}, 500);
@@ -44,7 +32,6 @@ $(function(){
 			$('#content').animate({left: "-=300"}, 500);
 		}
 	})	
-
 	// Anima o menu para uma transicao mais suave e caso o menu mobile esteja aberto fecha após o click
 	$('nav a').click(function(){
 		var href = $(this).attr('href');
@@ -57,9 +44,6 @@ $(function(){
 			$('#content').animate({left: "-=300"}, 500);
 		}
 		$('html, body').animate({'scrollTop':offSetTop});
-
 		return false;
 	});
-
 });
-
